d2c34ff73fb0ce409a9167842b06f3078eb79af5
Christian Versloot <mail@christianversloot.nl>
2019-10-17 21:20:42 +0200

    Add multiclass hinge

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ Examples of Keras loss functions
 * `huber-loss-outliers.py`: Using Huber loss in Keras - https://www.machinecurve.com/index.php/2019/10/12/using-huber-loss-in-keras/
 * `hinge-loss.py`: How to use hinge & squared hinge loss with Keras? - https://www.machinecurve.com/index.php/2019/10/15/how-to-use-hinge-squared-hinge-loss-with-keras/
 * `squared-hinge-loss.py`: How to use hinge & squared hinge loss with Keras? - https://www.machinecurve.com/index.php/2019/10/15/how-to-use-hinge-squared-hinge-loss-with-keras/
+* `multiclass-hinge.py`: How to use categorical / multiclass hinge with Keras? - https://www.machinecurve.com/index.php/2019/10/17/how-to-use-categorical-multiclass-hinge-with-keras/
---
+++ multiclass-hinge.py
@@ -0,0 +1,90 @@
+'''
+  Keras model discussing Categorical (multiclass) Hinge loss.
+'''
+import keras
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.utils import to_categorical
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.datasets import make_blobs
+from mlxtend.plotting import plot_decision_regions
+
+# Configuration options
+num_samples_total = 3000
+training_split = 1000
+num_classes = 3
+feature_vector_length = len(X_training[0])
+input_shape = (feature_vector_length,)
+loss_function_used = 'categorical_hinge'
+learning_rate_used = 0.03
+optimizer_used = keras.optimizers.adam(lr=learning_rate_used)
+additional_metrics = ['accuracy']
+num_epochs = 30
+batch_size = 5
+validation_split = 0.2 # 20%
+
+# Generate data
+X, targets = make_blobs(n_samples = num_samples_total, centers = [(0,0), (15,15), (0,15)], n_features = num_classes, center_box=(0, 1), cluster_std = 1.5)
+categorical_targets = to_categorical(targets)
+X_training = X[training_split:, :]
+X_testing = X[:training_split, :]
+Targets_training = categorical_targets[training_split:]
+Targets_testing = categorical_targets[:training_split].astype(np.integer)
+
+# Generate scatter plot for training data
+plt.scatter(X_training[:,0], X_training[:,1])
+plt.title('Three clusters ')
+plt.xlabel('X1')
+plt.ylabel('X2')
+plt.show()
+
+# Create the model
+model = Sequential()
+model.add(Dense(4, input_shape=input_shape, activation='relu', kernel_initializer='he_uniform'))
+model.add(Dense(2, activation='relu', kernel_initializer='he_uniform'))
+model.add(Dense(num_classes, activation='tanh'))
+
+# Configure the model and start training
+model.compile(loss=loss_function_used, optimizer=optimizer_used, metrics=additional_metrics)
+history = model.fit(X_training, Targets_training, epochs=num_epochs, batch_size=batch_size, verbose=1, validation_split=validation_split)
+
+# Test the model after training
+test_results = model.evaluate(X_testing, Targets_testing, verbose=1)
+print(f'Test results - Loss: {test_results[0]} - Accuracy: {test_results[1]*100}%')
+
+'''
+  The Onehot2Int class is used to adapt the model so that it generates non-categorical data.
+  This is required by the `plot_decision_regions` function.
+  The code is courtesy of dr. Sebastian Raschka at https://github.com/rasbt/mlxtend/issues/607.
+  Thanks!
+'''
+# No hot encoding version
+class Onehot2Int(object):
+
+    def __init__(self, model):
+        self.model = model
+
+    def predict(self, X):
+        y_pred = self.model.predict(X)
+        return np.argmax(y_pred, axis=1)
+
+# fit keras_model
+keras_model_no_ohe = Onehot2Int(model)
+
+# Plot decision boundary
+plot_decision_regions(X_testing, np.argmax(Targets_testing, axis=1), clf=keras_model_no_ohe, legend=3)
+plt.show()
+'''
+  Finish plotting the decision boundary.
+'''
+
+# Visualize training process
+plt.plot(history.history['loss'], label='Categorical Hinge loss (training data)')
+plt.plot(history.history['val_loss'], label='Categorical Hinge loss (validation data)')
+plt.title('Categorical Hinge loss for circles')
+plt.ylabel('Categorical Hinge loss value')
+plt.yscale('log')
+plt.xlabel('No. epoch')
+plt.legend(loc="upper left")
+plt.show()
